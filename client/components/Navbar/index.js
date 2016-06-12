9eff725ed4fdf31b8a0cdffa08f1d67a9485ae98
antoinechalifour <antoine.chalifour@gmail.com>
2016-06-12 20:34:08 +0200

    Added SMTP server in NavBar

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -5,13 +5,12 @@ export default class Navbar extends Component {
   render() {
     const style = require('./navbar.css')
     const { connected } = this.props
-    const status = connected ? `Connected to ${__SERVER__}` : 'Disconnected'
 
     return (
       <div className={style.navbar}>
         <h1>SMTP-dev-tool</h1>
         <p className={classnames(style.status, { [style.connected]: connected })}>
-          {status}
+          {`WS: http://${__SERVER__} | SMTP: ${__SMTP__}`}
         </p>
       </div>
     )
